44cf03571cf64573d88e1c78c6c4f74021c42ac1
vaseapinkov <wasea.pinkov@gmail.com>
2023-10-04 00:48:33 +0300

    Add jetstream

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', PageHomeController::class)->name('pages.home');
 Route::get('courses/{course:slug}', PageCourseDetailsController::class)->name('pages.course-details');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
